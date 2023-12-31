64b863476d8b667a48d5ef36264bb37fa59bcdfb
Danubio de Araujo <danubio.bwm@gmail.com>
2023-12-30 23:24:33 -0300

    test_routes_category_add

--- projeto2/app/main.py
+++ projeto2/app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.routes.category_routes import router as category_routes
 
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World2"}
+
+app.include_router(category_routes)
